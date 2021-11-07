02bb0e2f125be2d81582cf9d63ec5f9b92adb133
Shivang Gupta <86548591+shivanggupta24@users.noreply.github.com>
2021-11-07 10:45:02 +0530

    Create 1.2 (Best Time to Buy and Sell Stock).cpp

---
+++ Arrays/1.2 (Best Time to Buy and Sell Stock).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min_price = INT_MAX;
+        int profit = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            min_price = min(min_price,prices[i]);
+            profit = max(profit,prices[i] - min_price);
+        }
+        return profit;
+    }
+};
