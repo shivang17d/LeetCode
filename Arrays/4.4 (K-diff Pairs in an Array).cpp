9978e0328f8716038539892fad5218da10f10ccb
Shivang Gupta <shivangguptaxd@gmail.com>
2022-07-19 00:55:03 +0530

    Update 4.4 (K-diff Pairs in an Array).cpp

--- Arrays/4.4 (K-diff Pairs in an Array).cpp
+++ Arrays/4.4 (K-diff Pairs in an Array).cpp
@@ -1,3 +1,5 @@
+// Method 1 :
+
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
@@ -26,5 +28,22 @@ public:
         }
         return ans;
         
+    }
+};
+
+
+// Method 2 :
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if( k < 0) return 0;
+        unordered_map<int,int> m;
+        for(int n : nums) m[n]++;
+        int cnt = 0;
+        for(auto p : m) {
+            if((!k && p.second > 1) || (k && m.count(p.first + k))) ++cnt;
+        }
+        return cnt;
     }
 };
